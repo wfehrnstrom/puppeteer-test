650c39bee8f336a5d4c25518bb1c3fcc9b0640b2
Will Fehrnstrom <wfehrnstrom@gmail.com>
2021-02-26 20:57:33 -0500

    change trigger time.

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ let page_load = (async (pageName) => {
     const page = await browser.newPage();
     await page.setViewport({ width: 1720, height: 900});
     await page.goto(pageName);
+    console.log(Date.now().toTimeString());
   });
 
 
 let now = new Date();
 
-let target = Date.parse("2021-02-26T20:26:00");
+let target = Date.parse("2021-03-01T07:00:00");
 let pageUrl = 'https://www.publix.com/covid-vaccine/florida';
 if (target > now){
     // schedule a callback to execute
