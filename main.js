ef451c92f1e608c55cdcad45a582784d368011eb
Will Fehrnstrom <wfehrnstrom@gmail.com>
2021-03-02 13:58:39 -0500

    Add periodic resync.

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
 const puppeteer = require("puppeteer");
 
 puppeteer.launch({headless: false}).then((browser) => {
+
     let page_load = (async (pageName) => {
+        let start = new Date();
         const page = await browser.newPage();
+        let end0 = new Date();
         await page.setViewport({ width: 1720, height: 900});
+        let end1 = new Date();
         await page.goto(pageName);
-        console.log(new Date().toTimeString());
+        let end = new Date();
+        console.log(end0-start);
+        console.log(end1-end0);
+        console.log(end-end1);
+        if(intervalfire){
+            clearInterval(intervalfire);
+        }
       });
     
-    
-    let now = new Date();
-    
-    let target = Date.parse("2021-03-01T07:00:00");
+    pageload_trigger = null;
     let pageUrl = 'https://www.publix.com/covid-vaccine/florida';
-    if (target > now){
-        // schedule a callback to execute
-        let msRemaining = target - now;
-        setTimeout(page_load, msRemaining, pageUrl);
-    } else {
-        // execute
-        page_load(pageUrl);
-    }
+
+    let time_adjust_event = ((pageUrl) => {
+        let now = Date();
+        let target = Date.parse("2021-03-01T16:47:00");
+        if (target > now){
+            if(pageload_trigger){
+                clearTimeout(pageload_trigger);
+            }
+            let msRemaining = target - now;
+            pageload_trigger = setTimeout(page_load, msRemaining, pageUrl);
+        } else {
+            page_load(pageUrl);
+        }
+    });
+    
+    time_adjust_event(pageUrl);
+    intervalfire = setInterval(time_adjust_event, 10000, pageUrl);
 });
